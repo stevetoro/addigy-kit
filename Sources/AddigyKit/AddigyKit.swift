44da344f2be0b18c5fcce5e9c3724f69a1fd799a
Steve Toro <steve.toro@gmail.com>
2021-01-04 12:52:48 -0500

    /api/validate endpoint.

--- Sources/AddigyKit/AddigyKit.swift
+++ Sources/AddigyKit/AddigyKit.swift
@@ -1,15 +1,53 @@
 import Foundation
+import Combine
 
+@available(OSX 10.15, *)
 public class Addigy {
     let clientID: String
     let clientSecret: String
     let realm: String
     let session: URLSession
     
+    enum Errors: Error {
+        case invalidClientKeys
+    }
+    
     init(clientID: String, clientSecret: String, realm: String = "prod", session: URLSession = .shared) {
         self.clientID = clientID
         self.clientSecret = clientSecret
         self.realm = realm
         self.session = session
+    }
+    
+    public func validate() -> AnyPublisher<String, Error> {
+        struct ValidateResponse: Decodable {
+            let OrgID: String
+            
+            enum CodingKeys: String, CodingKey {
+                case OrgID = "orgid"
+            }
+        }
+        
+        var request = URLRequest(url: URL(string: "https://\(realm).addigy.com/api/validate")!)
+        request.httpMethod = "POST"
+        request.addValue(clientID, forHTTPHeaderField: "client-id")
+        request.addValue(clientSecret, forHTTPHeaderField: "client-secret")
+        
+        return session.dataTaskPublisher(for: request)
+            .tryMap() { element -> Data in
+                guard let response = element.response as? HTTPURLResponse else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                if response.statusCode == 401 {
+                    throw Addigy.Errors.invalidClientKeys
+                }
+                
+                return element.data
+            }
+            .decode(type: ValidateResponse.self, decoder: JSONDecoder())
+            .map(\.OrgID)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
 }
--- Tests/AddigyKitTests/AddigyKitTests.swift
+++ Tests/AddigyKitTests/AddigyKitTests.swift
@@ -1,8 +1,10 @@
 import XCTest
+import Combine
 
 @testable import AddigyKit
 
 final class AddigyKitTests: XCTestCase {
+    var subscriptions = Set<AnyCancellable>()
     
     func testInitializer() {
         var client = Addigy(clientID: "test-client-id", clientSecret: "test-client-secret")
@@ -16,8 +18,39 @@ final class AddigyKitTests: XCTestCase {
         XCTAssertEqual(client.realm, "dev")
         XCTAssertEqual(client.session, session)
     }
+    
+    func testValidate() {
+        let url = URL(string: "https://prod.addigy.com/api/validate")
+        let data = Data("{\"orgid\": \"test-org-id\"}".utf8)
+        URLProtocolStub.testURLs = [url: data]
+
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolStub.self]
+
+        let session = URLSession(configuration: config)
+        
+        let client = Addigy(
+            clientID: "test-client-id",
+            clientSecret: "test-client-secret",
+            session: session
+        )
+        
+        let expectation = XCTestExpectation(description: "validate")
+        client.validate()
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { value in
+                    XCTAssertEqual(value, "test-org-id")
+                    expectation.fulfill()
+                }
+            )
+            .store(in: &subscriptions)
+        
+        wait(for: [expectation], timeout: 1)
+    }
 
     static var allTests = [
-        ("testInitializer", testInitializer)
+        ("testInitializer", testInitializer),
+        ("testValidate", testValidate)
     ]
 }
