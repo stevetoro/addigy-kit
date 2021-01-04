d85c99d0aeaa1a5294a1530b0d56ebed3ebe9895
Steve Toro <steve.toro@gmail.com>
2021-01-04 12:51:55 -0500

    URLProtocol stub to intercept network requests.

---
+++ Tests/AddigyKitTests/stubs/URLProtocolStub.swift
@@ -0,0 +1,34 @@
+//
+//  URLProtocolStub.swift
+//  
+//
+//  Created by Steve Toro on 1/4/21.
+//
+
+import Foundation
+
+class URLProtocolStub: URLProtocol {
+    static var testURLs = [URL?: Data]()
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        if let url = request.url {
+            if let data = URLProtocolStub.testURLs[url] {
+                let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "HTTP/2.0", headerFields: ["data": data.base64EncodedString()])
+                self.client?.urlProtocol(self, didReceive: response!, cacheStoragePolicy: .notAllowed)
+                self.client?.urlProtocol(self, didLoad: data)
+            }
+        }
+
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() { }
+}
