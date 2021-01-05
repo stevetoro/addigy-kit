372145c7889b831f00247f4a1ec97d23ac861426
Steve Toro <steve.toro@gmail.com>
2021-01-05 08:30:25 -0500

    Extracted networking logic to generic function.

--- Sources/AddigyKit/AddigyKit.swift
+++ Sources/AddigyKit/AddigyKit.swift
@@ -20,52 +20,23 @@ public class Addigy {
     }
     
     public func validate() -> AnyPublisher<Token, Error> {
-        var request = URLRequest(url: URL(string: "https://\(realm).addigy.com/api/validate")!)
-        request.httpMethod = "POST"
-        request.addValue(clientID, forHTTPHeaderField: "client-id")
-        request.addValue(clientSecret, forHTTPHeaderField: "client-secret")
-        
-        return session.dataTaskPublisher(for: request)
-            .tryMap() { element -> Data in
-                guard let response = element.response as? HTTPURLResponse else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                if response.statusCode == 401 {
-                    throw Addigy.Errors.invalidClientKeys
-                }
-                
-                return element.data
-            }
-            .decode(type: Token.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        return call(endpoint: "https://\(realm).addigy.com/api/validate", method: "POST")
     }
     
     public func getDevices() -> AnyPublisher<[Device], Error> {
-        var request = URLRequest(url: URL(string:"https://\(realm).addigy.com/api/devices")!)
-        request.addValue(clientID, forHTTPHeaderField: "client-id")
-        request.addValue(clientSecret, forHTTPHeaderField: "client-secret")
-        
-        return session.dataTaskPublisher(for: request)
-            .tryMap() { element -> Data in
-                guard let response = element.response as? HTTPURLResponse else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                if response.statusCode == 401 {
-                    throw Addigy.Errors.invalidClientKeys
-                }
-                
-                return element.data
-            }
-            .decode(type: [Device].self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        return call(endpoint: "https://\(realm).addigy.com/api/devices")
     }
     
     public func getOnlineDevices() -> AnyPublisher<[Device], Error> {
-        var request = URLRequest(url: URL(string:"https://\(realm).addigy.com/api/devices/online")!)
+        return call(endpoint: "https://\(realm).addigy.com/api/devices/online")
+    }
+}
+
+@available(OSX 10.15, *)
+extension Addigy {
+    private func call<Type: Decodable>(endpoint: String, method: String = "GET") -> AnyPublisher<Type, Error> {
+        var request = URLRequest(url: URL(string: endpoint)!)
+        request.httpMethod = method
         request.addValue(clientID, forHTTPHeaderField: "client-id")
         request.addValue(clientSecret, forHTTPHeaderField: "client-secret")
         
@@ -81,9 +52,8 @@ public class Addigy {
                 
                 return element.data
             }
-            .decode(type: [Device].self, decoder: JSONDecoder())
+            .decode(type: Type.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
-
 }
